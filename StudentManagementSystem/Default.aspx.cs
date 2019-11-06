b12658afc3e2ee8423eb6d31c6b8a0ed5e57a397
Bishnu Rawal <Bishnu.Rawal@verscend.com>
2019-11-06 07:02:56 +0545

    Populate gridview from db table(ADO.NET  Basics)

---
+++ StudentManagementSystem/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StudentManagementSystem
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connString = @"Data Source=.\SQLEXPRESS;Initial Catalog=MBMDB;User ID=expressuser;Password=123456;Connect Timeout=30;";
+            SqlConnection sqlConnection = new SqlConnection(connString);
+            SqlCommand sqlCommand = new SqlCommand("Select * from Student", sqlConnection);
+            sqlConnection.Open();
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            GridView1.DataSource = sqlDataReader;
+            GridView1.DataBind();
+        }
+    }
+}
